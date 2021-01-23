824a9c9d114acba46e572308cab5762a3ce363f8
Gokul Viswanath <viswanath1gokul@gmail.com>
2021-01-23 11:30:22 +0530

    v2.0 Final
    Added favicon along with font and logo changes.

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ import os
 
 from imageoperations import upload_image, convert_image
 
+from rq import Queue
+from worker import conn
+
 app = Flask(__name__)
 jsglue = JSGlue(app)
 
+q = Queue(connection=conn
+          )  # create an RQ queue. Will be used for the uploadImage() function.
+
 os.environ["MAX_FILE_SIZE"] = "20"
+
 app.config['UPLOAD_FOLDER'] = '/tmp/'
 # app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 5 # 5MB size limit for uploading files.
 
@@ -21,25 +28,25 @@ def index(error=" "):
         error = request.form.get('error')
 
     return render_template('input.html',
-                           maxFileSize=os.environ.get("MAX_FILE_SIZE"), apiKey = os.environ.get("IMGUR_KEY"),
+                           maxFileSize=os.environ.get("MAX_FILE_SIZE"),
                            error=error)
 
 
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     if (request.method == 'POST'):
-#         if (is_base64(request.files.get('file'))):
-#             imgData = request.files.get('file')
-#         else:
-#             imgData = b64encode(request.files.get('file').read())
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if (request.method == 'POST'):
+        if (is_base64(request.files.get('file'))):
+            imgData = request.files.get('file')
+        else:
+            imgData = b64encode(request.files.get('file').read())
 
-#     # Start the upload as a background task in the Redis queue
-#     task = q.enqueue(upload_image, imgData)
+    # Start the upload as a background task in the Redis queue
+    task = q.enqueue(upload_image, imgData)
 
-#     # create a dictionary with the ID of the task
-#     responseObject = {"status": "success", "data": {"taskID": task.get_id()}}
-#     # return the dictionary
-#     return jsonify(responseObject)
+    # create a dictionary with the ID of the task
+    responseObject = {"status": "success", "data": {"taskID": task.get_id()}}
+    # return the dictionary
+    return jsonify(responseObject)
 
     # elif (request.form.get('type') == 'txt'):
     # outputtxt, errors = img2txt.main(str(inp['link']),
@@ -67,25 +74,25 @@ def index(error=" "):
     #                            errors=errors)
 
 
-# @app.route('/tasks/<taskID>', methods=['GET'])
-# def get_status(taskID):
-#     task = q.fetch_job(taskID)
+@app.route('/tasks/<taskID>', methods=['GET'])
+def get_status(taskID):
+    task = q.fetch_job(taskID)
 
-#     # If such a job exists, return its info
-#     if (task):
-#         responseObject = {
-#             "success": "success",
-#             "data": {
-#                 "taskID": task.get_id(),
-#                 "taskStatus": task.get_status(),
-#                 "taskResult": task.result
-#             }
-#         }
+    # If such a job exists, return its info
+    if (task):
+        responseObject = {
+            "success": "success",
+            "data": {
+                "taskID": task.get_id(),
+                "taskStatus": task.get_status(),
+                "taskResult": task.result
+            }
+        }
 
-#     else:
-#         responseObject = {"status": "error"}
+    else:
+        responseObject = {"status": "error"}
 
-#     return responseObject
+    return responseObject
 
 
 @app.route('/convert', methods=['POST'])
@@ -93,28 +100,35 @@ def convert_file():
     if (request.method == 'POST'):
 
         # Start the image conversion as a background task in the Redis Queue
-        responseObject = convert_image(str(request.form.get('imglink')),
+        task = q.enqueue(convert_image, str(request.form.get('imglink')),
                          str(request.form.get('type')),
                          str(request.form.get('mode')),
                          str(request.form.get('bg')),
                          int(request.values.get('num_cols')),
                          int(request.values.get('scale')))
 
-        
+        # create a dictionary with the ID of the task
+        responseObject = {
+            "status": "success",
+            "data": {
+                "taskID": task.get_id()
+            }
+        }
+
         # return the dictionary
-        return responseObject
+        return jsonify(responseObject)
 
 
-# def is_base64(file):
-#     try:
-#         return b64decode(file.encode('ascii')) == file
-#     except Exception:
-#         return False
+def is_base64(file):
+    try:
+        return b64decode(file.encode('ascii')) == file
+    except Exception:
+        return False
 
 
 @app.errorhandler(404)
 def request_page_not_found(error):
-    return '404', 404
+    return '404 not found', 404
 
 
 if __name__ == '__main__':
